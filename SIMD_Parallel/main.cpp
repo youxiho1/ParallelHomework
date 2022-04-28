9803cfc3963dc78f4811b8e19fc6bca14075e585
youxiho1 <2484451154@qq.com>
2022-04-28 23:21:55 +0800

    x86 complete

--- SIMD_Parallel/main.cpp
+++ SIMD_Parallel/main.cpp
@@ -7,9 +7,11 @@
 #include <time.h>
 
 using namespace std;
-const int N = 1000;
+const int N = 10000;
+const int M = 10;
 float input[3][N][N];
-float kernel[3][N][N]  ;
+float kernel[3][M][M];
+float result[N][N];
 int inputHeight, inputWidth, channels;
 int kernelHeight, kernelWidth;
 int outputHeight, outputWidth;
@@ -72,6 +74,12 @@ void randInit() {
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
                 input[c][i][j] = rand() %256;
+            }
+        }
+    }
+    for(int c = 0; c < 3; c++) {
+        for(int i = 0; i < M; i++) {
+            for(int j = 0; j < M; j++) {
                 kernel[c][i][j] = rand();
             }
         }
@@ -83,7 +91,7 @@ void conv() {
     //Calculate
     outputHeight = inputHeight - kernelHeight + 1;
     outputWidth = inputWidth - kernelWidth + 1;
-    float result[outputHeight][outputWidth];
+
     for(int i = 0; i < outputHeight; i++) {
         for(int j = 0; j < outputWidth; j++) {
             float sum = 0;
@@ -95,6 +103,47 @@ void conv() {
                 }
             }
             result[i][j] = sum;
+        }
+    }
+
+    //Print
+    for(int i = 0; i < outputHeight; i++) {
+        for(int j = 0; j < outputWidth; j++) {
+            printf("%.0f\t", result[i][j]);
+
+        }
+        putchar('\n');
+    }
+}
+
+void conv_simd() {
+    __m128 t1, t2, s;
+    //Calculate
+    outputHeight = inputHeight - kernelHeight + 1;
+    outputWidth = inputWidth - kernelWidth + 1;
+
+    for(int i = 0; i < outputHeight; i++) {
+        for(int j = 0; j < outputWidth; j++) {
+            float temp = 0;
+            s = _mm_setzero_ps();
+            for(int c = 0; c < channels; c++) {
+                for(int k = 0; k < kernelHeight; k++) {         //kernelWidth
+                    for(int t = kernelWidth - 4; t >= 0; t -= 4) {
+                        t1 = _mm_loadu_ps(input[c][i+k]+j+t);
+                        t2 = _mm_loadu_ps(kernel[c][k]+t);
+                        t1 = _mm_mul_ps(t1, t2);
+                        s = _mm_add_ps(s, t1);
+                    }
+
+                    for(int t = (kernelWidth % 4) - 1; t >= 0; t--) {
+                        temp += input[c][i+k][j+t] * kernel[c][k][t];
+                    }
+                }
+            }
+            s = _mm_hadd_ps(s, s);
+            s = _mm_hadd_ps(s, s);
+            _mm_store_ss(result[i]+j, s);
+            result[i][j] += temp;
         }
     }
 
@@ -112,21 +161,24 @@ void conv() {
 
 int main() {
 
-    int loops = 100;
-    channels = 1;
-    inputHeight = inputWidth = 10;
-    kernelHeight = kernelWidth = 4;
+    int args[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 5000, 10000};
+    for(int x = 0; x < 22; x++) {
+        int loops = 100;
+        channels = 1;
+        inputHeight = inputWidth = args[x];
+        kernelHeight = kernelWidth = 4;
 
-    //readFromFile();
-    randInit();
+        //readFromFile();
+        randInit();
 
-    Timer* timer = new Timer();
-    timer->Start();
-    for(int i = 0; i < loops; i++) {
-        conv();
+        Timer* timer = new Timer();
+        timer->Start();
+        for(int i = 0; i < loops; i++) {
+            conv_simd();
+        }
+        timer->Stop();
+        printf("%fms\n", timer->GetElapsedMilliseconds());
     }
-    timer->Stop();
-    printf("%fms\n", timer->GetElapsedMilliseconds());
 
     return 0;
 }
